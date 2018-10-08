819f1a4f6399a6b367a8eb0278569b1d58b6e6d3
Glitch (redux-interest-calculator-disconnected) <none>
2018-10-08 08:27:45 -0700

    finished first drill

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,36 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 // Connect this component
-export default function InterestCalculator(props) {
-    return (
-        <form className="interest-calculator"
-            onSubmit={e => e.preventDefault()}>
-            <div className="form-group">
-                <label htmlFor="principal">Principal ($)</label>
-                <input type="number" id="principal" value={props.principal}
-                    min="0" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="interest">Interest rate (%)</label>
-                <input type="number" id="interest" value={props.interest}
-                    min="0" max="100" step="0.1" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="years">Years</label>
-                <input type="number" id="years" value={props.years}
-                    min="0" max="100" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="total">Total</label>
-                <output type="number" id="total">${props.total.toFixed(2)}</output>
-            </div>
-        </form>
-    );
+function InterestCalculator(props) {
+  return (
+    <form className="interest-calculator" onSubmit={e => e.preventDefault()}>
+      <div className="form-group">
+        <label htmlFor="principal">Principal ($)</label>
+        <input type="number" id="principal" value={props.principal} min="0" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="interest">Interest rate (%)</label>
+        <input
+          type="number"
+          id="interest"
+          value={props.interest}
+          min="0"
+          max="100"
+          step="0.1"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="years">Years</label>
+        <input type="number" id="years" value={props.years} min="0" max="100" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="total">Total</label>
+        <output type="number" id="total">
+          ${props.total.toFixed(2)}
+        </output>
+      </div>
+    </form>
+  );
 }
 
 InterestCalculator.defaultProps = {
-    principal: 0,
-    interest: 0,
-    years: 0,
-    total: 0
+  principal: 0,
+  interest: 0,
+  years: 0,
+  total: 0
 };
+
+function mapStateToProps(state) {
+  return {
+    principal: state.principal,
+    interest: state.interest,
+    years: state.years,
+    total: state.principal * Math.pow(1 + state.interest / 100, state.years)
+  };
+}
+
+export default connect(mapStateToProps)(InterestCalculator);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import InterestCalculator from './components/interest-calculator';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-    <InterestCalculator />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <InterestCalculator />
+  </Provider>,
+  document.getElementById('root')
 );
